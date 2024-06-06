0b6486121b520d69444d8fa0a3a7987e396714fe
Rohit-web-dev <rohitpramanik77@gmail.com>
2024-06-06 09:17:02 +0530

    filter completed

---
+++ src/components/GenreFilter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const genres = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 27, name: 'Horror' },
+  { id: 53, name: 'Thriller' },
+  { id: 35, name: 'Comedy' },
+];
+
+const GenreFilter = ({ selectedGenres, onGenreChange }) => {
+  return (
+    <div className='p-4'>
+      <h4 className='text-lg font-semibold mb-2'>Filter by Genre</h4>
+      {genres.map((genre) => (
+        <div key={genre.id} className='mb-2'>
+          <input
+            type='checkbox'
+            id={`genre-${genre.id}`}
+            value={genre.id}
+            checked={selectedGenres.includes(genre.id)}
+            onChange={() => onGenreChange(genre.id)}
+          />
+          <label htmlFor={`genre-${genre.id}`} className='ml-2'>{genre.name}</label>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GenreFilter;
